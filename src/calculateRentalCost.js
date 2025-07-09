307006623a6e23a4d32ab360b215f324b1f262eb
Ann-ovocheviy <didicenkoanna00@gmail.com>
2025-07-09 19:53:13 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,19 +3,24 @@
  *
  * @return {number}
  */
+
+const DAILY_RATE = 40;
+const LONG_TERM = 7;
+const MEDIUM_TERM = 3;
+const MEDIUM_TERM_DISCOUNT = 20;
+const LONG_TERM_DISCOUNT = 50;
+
 function calculateRentalCost(days) {
   if (days <= 0) {
     return 0;
   }
 
-  const dailyRate = 40;
-
-  if (days < 3) {
-    return dailyRate * days;
-  } else if (days < 7 && days >= 3) {
-    return dailyRate * days - 20;
-  } else if (days >= 7) {
-    return dailyRate * days - 50;
+  if (days < MEDIUM_TERM) {
+    return DAILY_RATE * days;
+  } else if (days < LONG_TERM && days >= MEDIUM_TERM) {
+    return DAILY_RATE * days - MEDIUM_TERM_DISCOUNT;
+  } else if (days >= LONG_TERM) {
+    return DAILY_RATE * days - LONG_TERM_DISCOUNT;
   }
 }
 
